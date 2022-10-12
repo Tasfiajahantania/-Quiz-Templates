9d4d50aba83376d7f917e38712ec6d8979b936f3
Tasfiajahan <tania.develpers@gmail.com>
2022-10-12 12:50:28 +0600

    my 5th commit

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -5,13 +5,13 @@ import QuizQuestion from '../QuizQuestion/QuizQuestion';
 const QuizDetails = () => {
     const quizedetails = useLoaderData()
     const quizedetailsdata = quizedetails.data;
-    const {id, name, total, logo, questions} = quizedetailsdata;
+    const {id, name, total, questions} = quizedetailsdata;
 
     return (
         <div className='p-8'>
             <div className="p-3 flex flex-col justify-center border-r-2 col-span-2">
-                {/* <img className='w-64 m-auto' src={logo} alt="Quizs logo" /> */}
-                <h2 className='text-3xl font-bold mb-4 text-gray-900'>All {name} Quiz</h2>
+               
+                <h2 className='text-3xl font-bold mb-4 text-gray-900 underline'>All {name} Quiz</h2>
                 <h4 className='text-2xl font-medium mb-5'>Total Quiz {total}</h4>
                 {
                     questions.map(question => <QuizQuestion
