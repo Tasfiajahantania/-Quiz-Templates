c76db1164cade06452c2f623992f7cb1a678865f
Tasfiajahan <tania.develpers@gmail.com>
2022-10-12 22:20:48 +0600

    my 14th commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,16 +21,16 @@ const Header = () => {
             <div>
 
                 <nav className="flex justify-end items-center">
-                <div className={`flex justify-end md:flex-row sm:flex-col xs:flex-col md:static xs:bg-lime-600 md:bg-transparent xs:w-full absolute ${open ? 'top-20' : 'top-[-200px]'}`}>
+                <div className={`flex justify-end md:flex-row sm:flex-col xs:flex-col md:static xs:bg-blue-500 rounded p-4 md:bg-transparent xs:w-full absolute ${open ? 'top-20' : 'top-[-200px]'}`}>
                      <Link className='mr-4 text-xl text-cyan-100 hover:text-red-400' to='/home'>Home</Link>
                      <Link className='mr-4 text-xl text-cyan-100 hover:text-red-400' to='/statistic'>Statistics</Link>
                      <Link className='mr-4 text-xl text-cyan-100 hover:text-red-400' to='/blog'>Blog</Link>
                 </div>
                 <div className='md:hidden' onClick={() =>setOpen(!open)} >
                     <span>{open ? 
-                    <FontAwesomeIcon className='p-2 text-xl' icon={faXmark} /> 
+                    <FontAwesomeIcon className='p-2 text-xl text-white' icon={faXmark} /> 
                     : 
-                    <FontAwesomeIcon className='p-2 text-xl' icon={faBars} />}
+                    <FontAwesomeIcon className='p-2 text-x text-white' icon={faBars} />}
                     </span>
                 </div>
             </nav>
